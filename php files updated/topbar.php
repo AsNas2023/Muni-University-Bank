885a636cf029818bdd9c14e84ddebc253209665d
Apangu David <123071028+apangudavid@users.noreply.github.com>
2023-07-17 12:33:33 +0300

    Add files via upload

---
+++ php files updated/topbar.php
@@ -0,0 +1,27 @@
+<style>
+	.logo {
+    margin: auto;
+    font-size: 35px;
+   /* background: white;
+    padding: 7px 11px;
+    border-radius: 50% 50%;
+    color: #000000b3;*/
+}
+</style>
+<!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website : www.mayurik.com -->  
+<div id="page"></div>
+<div id="loading"></div>
+<nav class="navbar navbar-light fixed-top" style="padding:0;">
+  <div class="container-fluid mt-2 mb-2">
+  	<div class="col-lg-12 d-flex align-items-center justify-content-between">
+  		<div class="col-md-2 float-left" style="display: flex;">
+  			<div class="logo">
+  				<center><img src="images/lead.png" width="220px" style="object-fit:cover;object-position:center" width="100" height="50" class="d-inline-block align-top" alt=""></center>
+  			</div>
+  		</div>
+    </div>
+  </div>
+  
+</nav>
